f4f814325699df7060c8518bfcda274ddbe0d662
Swapnil Thatte <thatte.swapnil@gmail.com>
2022-03-28 14:44:46 +0530

    fifth commit

--- uber_api/uber/views.py
+++ uber_api/uber/views.py
@@ -1,9 +1,10 @@
 from django.http import JsonResponse
 from urllib import response
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-
+@csrf_exempt
 def newuser(request):
     if request.method == 'POST' :
         name = request.POST[name]
